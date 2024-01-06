fd63165a857a762fe460c55a027a2ffcad31b5a4
devb0x <moniezgeoffrey59@gmail.com>
2024-01-06 22:45:53 +0100

    add fetch random army for homepage - testing for deploy

--- app/api/dashboard/route.ts
+++ app/api/dashboard/route.ts
@@ -46,3 +46,4 @@ export const findUserArmies = async (ownerId) => {
 }
 
 
+
---
+++ app/api/homepage/route.ts
@@ -0,0 +1,13 @@
+import connect from "@/app/api/db"
+import {NextResponse} from "next/server"
+import Army from "@/app/models/army";
+
+export const findRandomArmy = async () => {
+	try {
+		await connect()
+	} catch (error) {
+		return new NextResponse('error in creation' + error)
+	}
+
+	return Army.find().limit(-1).skip(1)
+}
