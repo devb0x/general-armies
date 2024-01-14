07f8dcb001002831d985794aa4e44838e42b6951
devb0x <moniezgeoffrey59@gmail.com>
2024-01-14 21:33:49 +0100

    downgrade next js version for server action on netlify

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,28 +1,28 @@
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
-
-import { MongoDBAdapter } from "@auth/mongodb-adapter"
-import clientPromise from "../api/mongodb"
-
-export const { handlers, auth } = NextAuth({
-	providers:
-		[
-			GitHub({
-				allowDangerousEmailAccountLinking: true,
-			}),
-			GoogleProvider({
-				clientId: process.env.GOOGLE_ID,
-				clientSecret: process.env.GOOGLE_SECRET,
-				allowDangerousEmailAccountLinking: true,
-			})
-		],
-	secret: process.env.NEXTAUTH_SECRET,
-	adapter: MongoDBAdapter(clientPromise),
-	callbacks: {
-		async session({session, user}) {
-			session.user.id = user.id
-			return session
-		}
-	}
-})
+// import NextAuth from "next-auth"
+// import GitHub from "next-auth/providers/github"
+// import GoogleProvider from "next-auth/providers/google"
+//
+// import { MongoDBAdapter } from "@auth/mongodb-adapter"
+// import clientPromise from "../api/mongodb"
+//
+// export const { handlers, auth } = NextAuth({
+// 	providers:
+// 		[
+// 			GitHub({
+// 				allowDangerousEmailAccountLinking: true,
+// 			}),
+// 			GoogleProvider({
+// 				clientId: process.env.GOOGLE_ID,
+// 				clientSecret: process.env.GOOGLE_SECRET,
+// 				allowDangerousEmailAccountLinking: true,
+// 			})
+// 		],
+// 	secret: process.env.NEXTAUTH_SECRET,
+// 	adapter: MongoDBAdapter(clientPromise),
+// 	callbacks: {
+// 		async session({session, user}) {
+// 			session.user.id = user.id
+// 			return session
+// 		}
+// 	}
+// })
