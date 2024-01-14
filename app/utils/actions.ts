cad137b1e729ac2e8e33908f649e1627918a0272
devb0x <moniezgeoffrey59@gmail.com>
2024-01-14 17:53:01 +0100

    worst commit ever

--- app/utils/actions.ts
+++ app/utils/actions.ts
@@ -4,6 +4,10 @@ import {createUser, loginUser, searchUser} from "@/app/api/account/route"
 import { redirect } from "next/navigation"
 import {createArmy} from "@/app/api/dashboard/route"
 import {cookies} from "next/headers";
+import connect from "@/app/api/db";
+import User from "@/app/models/user";
+import bcrypt from "bcrypt";
+import {NextResponse} from "next/server";
 
 export  async function searchUserAction(formData: FormData) {
 	'use server'
@@ -29,15 +33,63 @@ export  async function searchUserAction(formData: FormData) {
 }
 
 export async function registerUserAction(formData: FormData) {
-	'use server'
+	// 'use server'
+	//
+	// const userName = formData.get("username")
+	// const userEmail = formData.get("email")
+	// const userPassword = formData.get("password")
+	//
+	// console.log('register user action called')
+	//
+	// return await createUser(userName, userEmail, userPassword)
 
-	const userName = formData.get("username")
-	const userEmail = formData.get("email")
-	const userPassword = formData.get("password")
+	const createUser = async (userName, userEmail, userPassword) => {
 
-	console.log('register user action called')
+		try {
+			await connect()
 
-	return await createUser(userName, userEmail, userPassword)
+			/**
+			 * hash password and save user
+			 */
+			const saltRounds = 10
+
+			bcrypt.genSalt(saltRounds, function (err, salt) {
+				bcrypt.hash(userPassword, salt, function (err, hash) {
+					// Store hash in your password DB.
+					const newUser = new User({
+						username: userName,
+						email: userEmail,
+						password: hash
+					})
+					return newUser.save(newUser)
+				})
+			})
+		} catch (error) {
+			return new NextResponse("error in saving newUser" + error, {status: 500})
+		}
+	}
+
+	// const { userName, userEmail, userPassword } = Object.fromEntries(formData)
+
+	try {
+		await connect()
+
+		// const userName = formData.get("username")
+		// const userEmail = formData.get("email")
+		// const userPassword = formData.get("password")
+		// const saltRounds = 10
+
+		// const salt = await bcrypt.genSalt(10);
+		// const hashedPassword = await bcrypt.hash(userPassword, 10);
+		const userName = formData.get("username")
+		const userEmail = formData.get("email")
+		const userPassword = formData.get("password")
+
+		await createUser(userName, userEmail, userPassword)
+	} catch (err) {
+		console.log(err)
+		throw new Error("Failed to create user.")
+	}
 }
 
 export async function loginUserAction(formData: FormData) {
