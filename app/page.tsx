b192750cc727edaa93e08384d40d7bac33fde57e
devb0x <moniezgeoffrey59@gmail.com>
2024-01-26 10:59:07 +0100

    removing spinner from homepage..

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,6 @@ export default async function Home(props) {
 
 		<main>
 			<h1>This is the homepage</h1>
-			<Spinner />
 			{/*<hr/>*/}
 			{/*{army.map((army, index) => (*/}
 			{/*	<ArmyCardLink*/}
